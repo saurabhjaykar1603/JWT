7c74f10ff1d4b300074b99f6d6f0eee6ebc885c4
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-12-28 14:00:27 +0530

    feat: implement user signup functionality

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,42 @@
 import User from "./../model/User.js";
-const postApiv1Signup = () => {};
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const postApiv1Signup = async (req, res) => {
+  try {
+    const { userName, email, password } = req.body;
+    if (!(userName && email && password)) {
+      res.status(400).json({
+        success: false,
+        message: "All Fields are Compulsory",
+      });
+    }
+   
+    // password enc
+    const enEncPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      userName,
+      email,
+      password: enEncPassword,
+    });
+    // generate token for user and send it
+    const token = jwt.sign(
+      { id: user._id, email },
+      process.env.JWT_SECRETE_KEY,
+      {
+        expiresIn: "3h",
+      }
+    );
+    user.token = token;
+    user.password = undefined;
+
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(401).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
 
 const postApiv1Login = () => {};
 export { postApiv1Signup, postApiv1Login };
