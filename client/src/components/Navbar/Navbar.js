f29f8144df3248060eb2a0ffe238df4f9033e9a6
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-12-29 09:18:14 +0530

    feat: implement user signup functionality

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-primary py-3 text-light sticky-top">
-        <div class="container-fluid">
-          <Link class="navbar-brand text-light fw-bold" to="/">
+      <nav className="navbar navbar-expand-lg navbar-light bg-primary py-3 text-light sticky-top">
+        <div className="container-fluid">
+          <Link className="navbar-brand text-light fw-bold" to="/">
             React JWT
           </Link>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -18,31 +18,31 @@ function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
                 <Link
-                  class="nav-link active text-light fw-bold"
+                  className="nav-link active text-light fw-bold"
                   aria-current="page"
                   to=""
                 >
                   Home
                 </Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link text-light fw-bold" to="/protected">
+              <li className="nav-item">
+                <Link className="nav-link text-light fw-bold" to="/protected">
                   Protected
                 </Link>
               </li>{" "}
-              <li class="nav-item">
-                <Link class="nav-link text-light fw-bold" to="/signup">
+              <li className="nav-item">
+                <Link className="nav-link text-light fw-bold" to="/signup">
                   Signup
                 </Link>
               </li>{" "}
-              <li class="nav-item">
-                <Link class="nav-link text-light fw-bold" to="/login">
+              <li className="nav-item">
+                <Link className="nav-link text-light fw-bold" to="/login">
                   Login
                 </Link>
               </li>
--- server/model/User.js
+++ server/model/User.js
@@ -1,7 +1,7 @@
 import { Schema, model } from "mongoose";
 const userSchema = new Schema(
   {
-    userName: { type: String, required: true, unique: true },
+    userName: { type: String, required: true},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
